cd233085be405dd9567e7adddd293506acef3b49
LoranMadec <laurent.lolo.madec@gmail.com>
2025-02-04 17:01:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
 
 from pandas.api.types import (
     is_categorical_dtype,
@@ -220,16 +221,31 @@ def rechercher_track(id_track):
 def generer_musique(prompt):
 
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Mode headless
-    chrome_options.add_argument("--no-sandbox")  
-    chrome_options.add_argument("--disable-dev-shm-usage")  
-    chrome_options.add_argument("--remote-debugging-port=9222") 
+    chrome_options.add_argument("--headless")  # Indispensable pour Streamlit
+    chrome_options.add_argument("--no-sandbox")  # *Essentiel* pour Streamlit
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Important pour Streamlit
+    chrome_options.add_argument("--remote-debugging-port=9222") # Ajoute ce port
+
+    # Chemin vers Chromium (CRUCIAL)
+    chromium_binary = "/usr/bin/chromium" # ou /usr/bin/chromium
+    chrome_options.binary_location = chromium_binary
+
+    service = Service(executable_path=chromium_binary) # Use Service object
+
+    browser = webdriver.Chrome(service=service, options=chrome_options)
+
+
+#    chrome_options = Options()
+#    chrome_options.add_argument("--headless")  # Mode headless
+#    chrome_options.add_argument("--no-sandbox")  
+#    chrome_options.add_argument("--disable-dev-shm-usage")  
+#    chrome_options.add_argument("--remote-debugging-port=9222") 
 #    chrome_options.add_argument("--no-sandbox")  # Mode headless
 #    chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Désactiver la détection automatisée
 #    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-    options = {"arguments": ["user-agent={user_agent}"]}
-    browser = start_chrome( options=chrome_options, headless=True)
+#   user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+#    options = {"arguments": ["user-agent={user_agent}"]}
+#    browser = start_chrome( options=chrome_options, headless=True)
 
     # Ouvrir le navigateur et aller sur le site riffusion
 #    browser = start_chrome()
@@ -239,7 +255,6 @@ def generer_musique(prompt):
     # Cliquez sur le bouton Continue with Discord
     click('Continue with Discord')
     time.sleep(2)
-
 
     # Remplir le formulaire avec E-mail = user et Password = pwd
     write(user, into='E-MAIL OU NUMÉRO DE TÉLÉPHONE*')
